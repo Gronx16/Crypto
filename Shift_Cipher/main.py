457d15228394be9ef19872e6d4da76df0262f634
Rohit Gupta <55290563+Gronx16@users.noreply.github.com>
2020-09-29 13:09:58 +0530

    Update main.py

--- Shift_Cipher/main.py
+++ Shift_Cipher/main.py
@@ -4,12 +4,12 @@ from decrypt import *
 # Driver function
 plane_text1 = input("Enter plane text: ").upper()  
 key1 = int(input("Enter Key: ")) 
-cipher_text1 = encrypt(plane_text1, key1)
+cipher_text1 = shift_encrypt(plane_text1, key1)
 print("Cipher text: " + cipher_text1)
 
 print("\n----------------------------------------------------\n")
 
 cipher_text2 = input("Enter cipher text: ").upper() 
 key2 = int(input("Enter Key: "))
-plane_text2 = decrypt(cipher_text2, key2)
+plane_text2 = shift_decrypt(cipher_text2, key2)
 print("Plane text: " + plane_text2)
